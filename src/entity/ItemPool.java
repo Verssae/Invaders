5805b4b0b7624f9e6f0e3ab3300f7f0e1c5d6199
khmrang2 <khmrang2@naver.com>
2023-10-08 21:55:41 +0900

    Specification Dscription of classes and deletion of unnecessary code

--- src/entity/ItemPool.java
+++ src/entity/ItemPool.java
@@ -4,15 +4,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ItemPool {
-
+    /** Set of already created items opearate like bulletpool*/
     private static Set<Item> pool = new HashSet<Item>();
 
+    /** not used constructor */
     private ItemPool() {
 
     }
-
+    /**
+     * Return an instance of item that can use ( or reuse ).
+     *
+     * @param positionX
+     *              PositionX of where item is first located.
+     * @param positionY
+     *              PositionY of where item is first located.s
+     *
+     * @return Item
+     * */
     public static Item getItem(final int positionX,
-                               final int positionY, final int speed) {
+                               final int positionY) {
         Item item;
         if (!pool.isEmpty()) {
             item = pool.iterator().next();
@@ -27,5 +37,10 @@ public class ItemPool {
         return item;
     }
 
+    /**
+     * Adds one or more bullets to the list of available ones.
+     * @param item
+     *              Items to recycle.
+     */
     public static void recycle(final Set<Item> item) { pool.addAll(item); }
 }
