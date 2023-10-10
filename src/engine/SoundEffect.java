c2629ce6987937a23fdc3322e15c6509908045e0
sseeoo81 <ing20184@gmail.com>
2023-10-10 18:19:15 +0900

    feat: create sound play methods

--- src/engine/SoundEffect.java
+++ src/engine/SoundEffect.java
@@ -1,7 +1,35 @@
 package engine;
 
+import java.io.File;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 public class SoundEffect implements SoundEffect_if {
 
+    File destroyedSound = new File("res/testsound.wav");
+    File missileShootingSound = new File("res/testsound.wav");
+
+    public void playMissileShootingSound() {
+        try{
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(missileShootingSound));
+            clip.start();
+
+            Thread.sleep(1);
+        }
+        catch(Exception e) { System.err.println(e); }
+    }
+
+    public void playDestroyedSound() {
+        try{
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(destroyedSound));
+            clip.start();
+
+            Thread.sleep(1);
+        }
+        catch(Exception e) { System.err.println(e); }
+    }
     public void SoundEffect_play(){
 
     }
