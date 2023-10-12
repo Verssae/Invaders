26e21851819c03f8b77bb3a178be34c254749d9b
Bigeco <bigeco173@gmail.com>
2023-10-13 05:35:07 +0900

    feat: add draw method about circle frame and  Horizontal KeyEvent

--- src/engine/Core.java
+++ src/engine/Core.java
@@ -234,8 +234,7 @@ public final class Core {
 								gameState.getHardCore());
 						
 						
-								// SubMenu : Item Store / Enhancement / Continue
-
+						// SubMenu : Item Store / Enhancement / Continue
 						do{
 							if (gameState.getLivesRemaining() <= 0) { break; }
 							if (!boxOpen){
