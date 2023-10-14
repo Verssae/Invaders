2e6a911afdec28bf3695a88a22037ac9cc7de8aa
user <sk87732925@gmail.com>
2023-10-14 18:26:43 +0900

    change the method of recovering

--- src/screen/GameScreen_2P.java
+++ src/screen/GameScreen_2P.java
@@ -85,8 +85,6 @@ public class GameScreen_2P extends Screen {
      *            Current game state.
      * @param gameSettings
      *            Current game settings.
-     * @param bonusLife
-     *            Checks if a bonus life is awarded this level
      * @param width
      *            Screen width.
      * @param height
@@ -95,17 +93,17 @@ public class GameScreen_2P extends Screen {
      *            Frames per second, frame rate at which the game is run.
      */
     public GameScreen_2P(final GameState gameState,
-                         final GameSettings gameSettings, final boolean bonusLife,
+                         final GameSettings gameSettings,
                          final int width, final int height, final int fps) {
         super(width, height, fps);
 
         this.gameSettings = gameSettings;
-        this.bonusLife = bonusLife;
+        //this.bonusLife = bonusLife;
         this.level = gameState.getLevel();
         this.score = gameState.getScore();
         this.lives = gameState.getLivesRemaining();
-        if (this.bonusLife)
-            this.lives++;
+        //if (this.bonusLife)
+            //this.lives++;
         this.bulletsShot = gameState.getBulletsShot();
         this.shipsDestroyed = gameState.getShipsDestroyed();
         this.hardcore = gameState.getHardCore();
@@ -416,6 +414,7 @@ public class GameScreen_2P extends Screen {
                     this.score += this.enemyShipSpecial.getPointValue();
                     this.shipsDestroyed++;
                     this.enemyShipSpecial.destroy(this.items);
+                    if (this.lives < 2.9) this.lives = this.lives + 0.1;
                     this.enemyShipSpecialExplosionCooldown.reset();
                     recyclableBullet.add(bullet);
                 }
