47e6bd516ccf0b00ec55b64f92d33a2b23cd6cf5
wwwept <wwwept@hanyang.ac.kr>
2023-10-17 15:20:24 +0900

    Refactor: refactor addCoin, minusCoin

--- src/entity/Coin.java
+++ src/entity/Coin.java
@@ -1,16 +1,19 @@
 package entity;
+import java.util.Random;
 
 public class Coin {
 
     public int coin = 0;
 
-    public int addCoin(int addCoin) {
+    public int addCoin() {
+        Random random = new Random();
+        int addCoin = random.nextInt(1, 11); //
         coin += addCoin;
         return coin;
     }
 
-    public int minusCoin(int minusCoin) {
-        coin -= minusCoin;
+    public int minusCoin(int cost) { // when buy an item
+        coin -= cost;
         return coin;
     }
 
