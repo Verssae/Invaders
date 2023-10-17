1b55877cdfad85e16bb0b58a3a02f33d603d3aed
domini251 <kdm030901@gmail.com>
2023-10-17 17:11:27 +0900

    error solved

--- src/screen/GameScreen.java
+++ src/screen/GameScreen.java
@@ -153,7 +153,6 @@ public class GameScreen extends Screen {
 
 		soundEffect = new SoundEffect();
 		bgm = new BGM();
-		drawManager.initBackgroundTimer();
 
 		drawManager.initBackgroundTimer(this, SEPARATION_LINE_HEIGHT); // Initializes timer for background animation.
 
