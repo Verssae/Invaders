63f1e3b4176fdf4587beb355c59d74b89cf01010
sunivu <sunivu@hanyang.ac.kr>
2023-11-12 01:54:18 +0900

    SEC-57, SEC-58 complete

--- src/engine/Core.java
+++ src/engine/Core.java
@@ -737,8 +737,28 @@ public final class Core {
                 case 5:
                     // Setting Screen.
                     currentScreen = new SettingScreen(width, height, FPS);
-                    LOGGER.info("Starting" + WIDTH + "x" + "Setting Screen Menu at" + FPS + "fps.");
+                    LOGGER.info("Starting " + WIDTH + "x" + HEIGHT + " Setting Screen Menu at " + FPS + " fps.");
                     int settingscreen = frame.setScreen(currentScreen);
+                    if (settingscreen == 101)
+                    {
+                        currentScreen = new SettingScreen_Sound(width, height, FPS);
+                        LOGGER.info("Open " + WIDTH + "x" + HEIGHT
+                                + " Sound Setting Screen" + FPS + " fps");
+                        returnCode = frame.setScreen(currentScreen);
+                        LOGGER.info("Closing Sound Setting Screen");
+                    }
+                    else if (settingscreen == 102)
+                    {
+                        currentScreen = new SettingScreen_Key(width, height, FPS);
+                        LOGGER.info("Open " + WIDTH + "x" + HEIGHT
+                                + " Key Setting Screen" + FPS + " fps");
+                        returnCode = frame.setScreen(currentScreen);
+                        LOGGER.info("Closing Key Setting Screen");
+                    }
+                    else
+                        returnCode = frame.setScreen(currentScreen);
+                    LOGGER.info("Closing Setting Menu Screen");
+                    break;
 
                 default:
                     break;
