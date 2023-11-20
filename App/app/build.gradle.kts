6a3f47d0607d1f285b2a973d37174875f82ec930
sseeoo81 <ing20184@gmail.com>
2023-11-20 18:44:15 +0900

    fix: change compileSdk(33->34)

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.invaders.invadersapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.invaders.invadersapp"
