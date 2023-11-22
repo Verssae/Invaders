2e203cd3881a57338e01a392b2c9a841a13ed1a5
sseeoo81 <ing20184@gmail.com>
2023-11-22 17:09:44 +0900

    feat: set Button variables from id and onClick methods

--- App/app/src/main/java/com/invaders/invadersapp/MainActivity.java
+++ App/app/src/main/java/com/invaders/invadersapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView title = (TextView) findViewById(R.id.title);
         title.setTextColor(Color.parseColor("#00ff00"));
+
+        Button play = (Button) findViewById(R.id.play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        Button itemstore = (Button) findViewById(R.id.itemstore);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
