94bbb8468193ee4c44294ab9898190474815f5f4
sseeoo81 <ing20184@gmail.com>
2023-11-23 03:08:24 +0900

    feat: add select effect

--- App/app/src/main/java/com/invaders/invadersapp/BlinkingRunnable.java
+++ App/app/src/main/java/com/invaders/invadersapp/BlinkingRunnable.java
@@ -1,29 +1,46 @@
 package com.invaders.invadersapp;
 
-import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.List;
 
 public class BlinkingRunnable extends MainActivity implements Runnable {
-    TextView[] textView;
-    String[] color;
+
+    private TextView[] tv;
+    public String[] colors;
+    private String[] colorsInit;
+    private int count = 0;
+    private int changedidx;
     public BlinkingRunnable(TextView[] t, String[] c) {
-        textView = t;
-        color = c;
+        tv = t;
+        colors = c;
+        colorsInit = new String[c.length];
+        for (int i = 0; i < c.length; i++) {
+            colorsInit[i] = c[i];
+        }
     }
     @Override
     public void run() {
         while (true) {
-            for (int i = 0; i < textView.length; i++) {
-                textView[i].setTextColor(blinkingColor(color[i]));
+            for (int i = 0; i < tv.length; i++) {
+                tv[i].setTextColor(blinkingColor(colors[i]));
+            }
+            if (count == 1) {
+                colors[changedidx] = colorsInit[changedidx];
+                count--;
+            }
+            else if (count > 1) {
+                count --;
             }
             try {
                 Thread.sleep(10);
-            } catch (Exception e) {
-                e.printStackTrace() ;
+            } catch (InterruptedException e) {
+                e.getStackTrace();
             }
         }
     }
-
+    public void changeColor(int idx, String color) {
+        changedidx = idx;
+        colors[changedidx] = color;
+        count = 30;
+    }
 }
