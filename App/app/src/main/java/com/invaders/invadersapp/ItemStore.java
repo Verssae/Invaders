53a305b81e1a79c7068a4bff2d0fa828761ad8c2
sseeoo81 <ing20184@gmail.com>
2023-11-22 17:17:41 +0900

    feat: set SelectDifficulty.java and ItemStore.java

---
+++ App/app/src/main/java/com/invaders/invadersapp/ItemStore.java
@@ -0,0 +1,15 @@
+package com.invaders.invadersapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ItemStore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.itemstore);
+    }
+}
