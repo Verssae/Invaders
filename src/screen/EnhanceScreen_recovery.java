ae952d53e45ee6af12effe257d9a91f75e17d46e
MinseoKim <137522122+DoggiKim@users.noreply.github.com>
2023-11-27 01:21:40 +0900

    SI_16 feat: add EnhanceScreen_recovery

---
+++ src/screen/EnhanceScreen_recovery.java
@@ -0,0 +1,112 @@
+package screen;
+
+import java.awt.event.KeyEvent;
+
+import engine.Cooldown;
+import engine.Core;
+import engine.SoundEffect;
+
+public class EnhanceScreen_recovery extends Screen {
+    /** Milliseconds between changes in user selection. */
+    private static final int SELECTION_TIME = 200;
+    /** Time between changes in user selection. */
+    private Cooldown selectionCooldown;
+    /** For selection moving sound */
+    private SoundEffect soundEffect;
+
+
+    /**
+     * Constructor, establishes the properties of the screen.
+     * 
+     * @param gameState
+     *                  Current game state.
+     * @param width
+     *                  Screen width.
+     * @param height
+     *                  Screen height.
+     * @param fps
+     *                  Frames per second, frame rate at which the game is run.
+     */
+    public EnhanceScreen_recovery(int width, int height, int fps) {
+        super(width, height, fps);
+
+        // Defaults to play.
+        this.returnCode = 2;
+        this.selectionCooldown = Core.getCooldown(SELECTION_TIME);
+        this.selectionCooldown.reset();
+
+        soundEffect = new SoundEffect();
+    }
+
+    /**
+     * Starts the action.
+     *
+     * @return Next screen code.
+     */
+    public final int run() {
+        super.run();
+
+        return this.returnCode;
+    }
+
+    /**
+     * Updates the elements on screen and checks for events.
+     */
+    protected final void update() {
+        super.update();
+
+        draw();
+        if (this.selectionCooldown.checkFinished()
+                && this.inputDelay.checkFinished()) {
+            if (inputManager.isKeyDown(KeyEvent.VK_UP)
+                    || inputManager.isKeyDown(KeyEvent.VK_W)) {
+                soundEffect.playButtonClickSound();
+                previousMenuItem();
+                this.selectionCooldown.reset();
+            }
+            if (inputManager.isKeyDown(KeyEvent.VK_DOWN)
+                    || inputManager.isKeyDown(KeyEvent.VK_S)) {
+                soundEffect.playButtonClickSound();
+                nextMenuItem();
+                this.selectionCooldown.reset();
+            }
+            if (inputManager.isKeyDown(KeyEvent.VK_SPACE)){
+                soundEffect.playSpaceButtonSound();
+                this.isRunning = false;
+            }
+        }
+    }
+
+    /**
+     * Shifts the focus to the next menu item.
+     */
+    private void nextMenuItem() {
+        if (this.returnCode == 7)
+            this.returnCode = 2;
+        else
+            this.returnCode = 7;
+    }
+
+    /**
+     * Shifts the focus to the previous menu item.
+     */
+    private void previousMenuItem() {
+        if (this.returnCode == 2)
+        this.returnCode = 7;
+    else
+        this.returnCode = 2;
+    }
+
+    /**
+     * Draws the elements associated with the screen.
+     */
+    private void draw() {
+        drawManager.initDrawing(this);
+
+        // drawManager.drawTitle(this);
+        drawManager.drawSubMenu(this, this.returnCode);
+
+        drawManager.completeDrawing(this);
+    }
+}
+
