6ffaeb23999421321cdb3de429b760a609734698
daxnii <stgyals3135@hanyang.ac.kr>
2023-11-28 20:51:27 +0900

    feat:set levelselection page and set level selection button

---
+++ App/app/src/main/java/com/invaders/invadersapp/selectlevel.java
@@ -0,0 +1,75 @@
+package com.invaders.invadersapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+public class selectlevel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selectlevel);
+
+        // 레벨 1 버튼
+        Button btnLevel1 = findViewById(R.id.btnLevel1);
+        btnLevel1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 레벨 1이 선택되었을 때의 동작
+
+            }
+        });
+
+        Button btnLevel2 = findViewById(R.id.btnLevel2);
+        btnLevel2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 레벨 2이 선택되었을 때의 동작
+
+            }
+        });
+
+        Button btnLevel3 = findViewById(R.id.btnLevel3);
+        btnLevel3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 레벨 3이 선택되었을 때의 동작
+
+            }
+        });
+
+        Button btnLevel4 = findViewById(R.id.btnLevel4);
+        btnLevel4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 레벨 4가 선택되었을 때의 동작
+
+            }
+        });
+
+        Button btnLevel5 = findViewById(R.id.btnLevel5);
+        btnLevel5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 레벨 5가 선택되었을 때의 동작
+
+            }
+        });
+
+        Button btnLevel6 = findViewById(R.id.btnLevel6);
+        btnLevel6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 레벨 6이 선택되었을 때의 동작
+
+            }
+        });
+    }
+}
