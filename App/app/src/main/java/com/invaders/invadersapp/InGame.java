b928e19d7877f6f3ba28dab9f77d2e9e9d7af6de
sseeoo81 <ing20184@gmail.com>
2023-11-30 01:03:48 +0900

    style: annotate

--- App/app/src/main/java/com/invaders/invadersapp/InGame.java
+++ App/app/src/main/java/com/invaders/invadersapp/InGame.java
@@ -1,7 +1,5 @@
 package com.invaders.invadersapp;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -14,7 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InGame extends AppCompatActivity {
-    private double shipPosition = 0.5;
+    /** ImageView of ship */
     private ImageView ship;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,20 +22,20 @@ public class InGame extends AppCompatActivity {
         ImageView left_icon = (ImageView) findViewById(R.id.left_icon);
         ImageView right_icon = (ImageView) findViewById(R.id.right_icon);
         ship = (ImageView) findViewById(R.id.ship);
-
+        // set img on left and right buttons
         left_icon.setImageResource(R.drawable.left_button);
         right_icon.setImageResource(R.drawable.right_button);
-
-        Log.i("Position", ship.getX()+"");
 
         left_icon.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // change left button green when it touched
                     left_icon.setImageResource(R.drawable.left_touched);
                     handlerLeft.post(runnableLeft);
                 }
                 if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // change left button white
                     left_icon.setImageResource(R.drawable.left_button);
                     handlerLeft.removeCallbacks(runnableLeft);
                 }
@@ -49,10 +47,12 @@ public class InGame extends AppCompatActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // change right button green when it touched
                     right_icon.setImageResource(R.drawable.right_touched);
                     handlerRight.post(runnableRight);
                 }
                 if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // change right button white
                     right_icon.setImageResource(R.drawable.right_button);
                     handlerRight.removeCallbacks(runnableRight);
                 }
@@ -60,27 +60,27 @@ public class InGame extends AppCompatActivity {
             }
         });
     }
+    /** Handler for left button to control runnableLeft */
     private Handler handlerLeft = new Handler(Looper.getMainLooper());
+    /** Runnable to move ship left */
     private Runnable runnableLeft = new Runnable() {
         @Override
         public void run() {
             if (ship.getX()-8 > 0) ship.setX(ship.getX()-8);
             else ship.setX(0);
-            Log.i("Position", ship.getX()+"");
-            // 쉽 무빙
-            // Call the runnable again
-            handlerLeft.postDelayed(this, 17);
+            handlerLeft.postDelayed(this, 17); // fps = 1000/17
         }
     };
+    /** Handler for right button to control runnableRight */
     private Handler handlerRight = new Handler(Looper.getMainLooper());
+    /** Runnable to move ship right */
     private Runnable runnableRight = new Runnable() {
         @Override
         public void run() {
             if (ship.getX()+8 < 1008) ship.setX(ship.getX()+8);
             else ship.setX(1008);
             Log.i("Position", ship.getX()+"");
-            // Call the runnable again
-            handlerRight.postDelayed(this, 17);
+            handlerRight.postDelayed(this, 17); // fps = 1000/17
         }
     };
 }
