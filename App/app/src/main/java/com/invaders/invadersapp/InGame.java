30f9d9a369d5506c51f2649059cf23a901bed575
sseeoo81 <ing20184@gmail.com>
2023-11-29 21:43:07 +0900

    feat: set icons on InGame.java and ingame.xml

--- App/app/src/main/java/com/invaders/invadersapp/InGame.java
+++ App/app/src/main/java/com/invaders/invadersapp/InGame.java
@@ -1,4 +1,21 @@
 package com.invaders.invadersapp;
 
-public class InGame {
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InGame extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ingame);
+
+        ImageView left_icon = (ImageView) findViewById(R.id.left_icon);
+        ImageView right_icon = (ImageView) findViewById(R.id.right_icon);
+
+    }
 }
